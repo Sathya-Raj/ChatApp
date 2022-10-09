ac9c957c69d05e8a7b2600d3fc0488de8d2a34e0
Sathya-Raj <sathyarajsaliyan@gmail.com>
2022-10-10 00:32:28 +0530

    Login page setup

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,10 +1,72 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import { useState } from 'react'
 
 function Join() {
+  const [name, setname] = useState("")
+  const [room, setroom] = useState("")
+
   return (
-    <div>Join
-      hellooo
+    <>
+    <section className="h-screen">
+  <div className="px-6 h-full text-gray-800">
+    <div
+      className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6"
+    >
+      <div
+        className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0"
+      >
+        <img
+          src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+          className="w-full"
+          alt="Sample image"
+        />
+      </div>
+      <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
+        <form>
+          <div
+            className="flex items-center my-8 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
+          >
+            <p className="text-center text-4xl font-semibold mx-4 mb-0">Chatify</p>
+          </div>
+          <div className="mb-6">
+            <input
+              type="text"
+              className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="exampleFormControlInput2"
+              placeholder="User Name"
+              onChange={(e)=>setname(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-6">
+            <input
+              type="password"
+              className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="exampleFormControlInput2"
+              placeholder="Room code"
+              onChange={(e)=>setroom(e.target.value)}
+            />
+          </div>
+
+
+
+          <div className="text-center lg:text-left mb-9">
+            <Link onClick={e=>(!name||!room)?e.preventDefault():null} to={`/chat?name=${name}&room=${room}`}>
+              <button
+                type="button"
+                className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+              >
+                Login
+              </button>
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
+  </div>
+</section>
+</>
   )
 }
 

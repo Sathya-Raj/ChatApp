3dd334ff12a809acae59542bee97212861db6959
Sathya-Raj <sathyarajsaliyan@gmail.com>
2022-10-09 23:37:18 +0530

    connection setup

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
+const router= require("./router");
 
 const PORT = process.env.PORT|| 5000;
 
 const app = express();
 const server = http.createServer(app);
 const io =  socketio(server);
+app.use(router);
+
+io.on('connection',(scoket)=>{
+    console.log("We have a new connection!!");
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+});
+
+
 
 server.listen(PORT,() => console.log(`Server running on port : ${PORT}`));

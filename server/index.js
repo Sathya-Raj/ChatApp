bcd6f0bfc638d1c951715bf45431619099484153
Sathyaraj Saliyan <96201509+Sathya-Raj@users.noreply.github.com>
2022-12-29 23:54:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 app.use(cors());
 const io = new Server(server,{
   cors:{
-    origin: "http://localhost:3000",
+    origin: "*",
     methods:["GET","POST"],
   },
 });
